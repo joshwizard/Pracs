947a1c8c9e583b44b3d6b08aeb811157a8582911
joshwizard <murikij@gmail.com>
2025-10-06 11:27:17 +0300

    Pracs

--- index.js
+++ index.js
@@ -675,26 +675,106 @@
 
 // addEventListenerToDeleteButtons()
 
-document.addEventListener('DOMContentLoaded', () => {
-    let form = document.querySelector('form')
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        handleInput(e.target.newInput.value)
-        form.reset()
-    })
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//     let form = document.querySelector('form')
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault()
+//         handleInput(e.target.newInput.value)
+//         form.reset()
+//     })
+// })
 
-function handleInput(input) {
-    let p = document.createElement('p')
-    let btn = document.createElement('button')
-    btn.addEventListener('click', handleDelete)
-    p.textContent = `${input} `
-    btn.textContent = 'x'
-    p.appendChild(btn)
-    console.log(p)
-    document.querySelector('#container').appendChild(p)
-}
+// function handleInput(input) {
+//     let p = document.createElement('p')
+//     let btn = document.createElement('button')
+//     btn.addEventListener('click', handleDelete)
+//     p.textContent = `${input} `
+//     btn.textContent = 'x'
+//     p.appendChild(btn)
+//     console.log(p)
+//     document.querySelector('#container').appendChild(p)
+// }
 
-function handleDelete(e) {
-    e.target.parentNode.remove()
-}
+// function handleDelete(e) {
+//     e.target.parentNode.remove()
+// }
+
+// Context and Expliciting -------------------
+// const byronPoodle = {
+//     name: "Byron",
+//     ageInYears: 2,
+//     warn: function () {
+//         console.log(`Bark bark bark`)
+//         console.log(this)
+//     }
+// }
+
+// // console.log(byronPoodle.name)
+// // console.log(byronPoodle.ageInYears)
+// byronPoodle.warn()
+
+// const blakeDoodle = {
+//     name: "Blake",
+//     breed: "Labradoodle",
+//     sonicAttack: "ear-rupturing atomic bark",
+//     mostHatedThing: "noises in the apartment hallway",
+//     warn: function() {
+//         console.log(`${this.name} the ${this.breed} issues an ${this.sonicAttack} when he hears ${this.mostHatedThing}`)
+//     }
+// }
+
+// blakeDoodle.warn()
+
+// const frog = { name: "Kermit" }
+// const pig = { name: "Miss Piggy" }
+// const speak = function() {
+//     return `It ain't easy being ${this.name}`
+// }
+// frog.speak = speak
+// pig.speak = speak
+
+// frog.speak === pig.speak
+
+// frog.speak()
+
+// const locationReturner = function () {
+//     return this.locationReturner
+// }
+
+// locationReturner()
+
+// function a() {
+//   return function b() {
+//     return this;
+//   }
+// }
+// a()() === global
+
+// function looseGoosey() {
+//     return this
+// }
+
+// function noInferringAllowed() {
+//     "use Strict"
+//     return this
+// }
+
+// looseGoosey() === global
+
+
+// class Poodle {
+//     constructor(name, pronoun) {
+//         this.name = name;
+//         this.pronoun = pronoun;
+//         this.sonicAttack = "ear-rupturing atomic bark";
+//         this.mostHatedThing = "noises in the apartment hallway";
+//     }
+
+//     warn() {
+//         console.log(`${this.name} issues an ${this.sonicAttack} when ${this.pronoun} hears ${this.mostHatedThing}`)
+//     }
+// }
+
+// const byron = new Poodle ("Byron", "he")
+// const blake = new Poodle ("Blake", "she")
+// byron.warn()
